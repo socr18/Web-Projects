16db71094e9017f3fa06c83c0bd14a9b9b606508
socr18 <jpolson@marketpath.com>
2016-03-05 10:52:27 -0500

    my daily update just in case

--- Marketpath/js/test.js
+++ Marketpath/js/test.js
@@ -1,74 +1,93 @@
-<script src="https://az480170.vo.msecnd.net/e2ce2d76-02ff-4ebc-8e64-519164387db4/docs/60591976-d94c-49ed-acd3-52ef4bfd281d/jquery.bxslider.js" ></script>
+</script>
+<script src = "https://az480170.vo.msecnd.net/e2ce2d76-02ff-4ebc-8e64-519164387db4/docs/60591976-d94c-49ed-acd3-52ef4bfd281d/jquery.bxslider.js"></script>
 <script>
-    var arr = [];
 
-$(document).ready(function () {
-            $('.bxslider').bxSlider({
-                auto: false,
-                mode: 'horizontal',
-                minSlides: 3,
-                maxSlides: 3,
-                moveSlides: 1
-            });
+var arr = [];
+var filter = [];
 
-            function gallery(filter) {
+function gallery(filter) {
 
-                var success = function(data) {
+    var success = function (data) {
 
-                        //data downloaded so we call parseJSON function
-                        //and pass downloaded data
-                            var json = $.parseJSON(data);
-                            for( var i = 0; i < json.length; i++) {
-                                //console.log(json);
-                                //now json variable contains data in json format
-                                //let's display a few items
-                                $('.bxslider2').append('<li> <a href="' + json[i]['page-url'] + '"><img src="' + json[i]['website-url'] + '" alt="' + json[i]['client-name'] + '" /> </a></li>');
-                            }
+        //data downloaded so we call parseJSON function
+        //and pass downloaded data
 
-                            $('.bxslider2').bxSlider({
-                                auto: true,
-                                mode: 'fade',
-                                speed: 4000
-                            });
-                        };
+        var json = $.parseJSON(data);
+        arr = $.parseJSON(data);
 
-                var successFilter = function (data,filter) {
+        filter = 'healthcare';
 
-                        //data downloaded so we call parseJSON function
-                        //and pass downloaded data
+        for (var i = 0; i < json.length; i++) {
 
-                            var json = $arr.parseJSON(data);
-                            for( var i = 0; i < arr.length; i++) {
-                                console.log(filter);
-                                console.log(arr);s
-                                //now json variable contains data in json format
-                                //let's display a few items
-                                 $('.bxslider2').append('<li> <a href="' + json[i]['page-url'] + '"><img src="' + json[i]['website-url'] + '" alt="' + json[i]['client-name'] + '" /> </a></li>');
-                            }
+            //add images to gallery
 
-                            $('.bxslider2').bxSlider({
-                                auto: true,
-                                mode: 'fade',
-                                speed: 4000
-                            });
-                        };
+            if (JSON.stringify(arr[i]['industry']).indexOf(filter) > 0) {
 
-                if ( arr == null || arr.length == 0 )
-                    {
-                //start ajax request
-                    $.ajax({
-                        url: "//mp-site.dynamic.marketpath.com/get/directory-data/4a743f66-194b-4ddb-aa5f-c148f51d55d8",
-                        //force to handle it as text
-                        dataType: "text",
-                        success: success
-                    });
-                    }
-                else{
-                    successFilter(filter);
-                }
+                $('.bxslider2').append('<li> <a href="' + json[i]['page-url'] + '"><img src="' + json[i]['website-url'] + '" alt="' + json[i]['client-name'] + '" /> </a></li>');
 
             }
 
-            gallery();
+        }
+
+        $('.bxslider2').bxSlider({
+            auto: true,
+                    mode: 'fade',
+                    speed: 4000
+        });
+
+    }
+
+    function successFilter(filter) {
+
+        $('.bxslider2').empty();
+
+        for (var i = 0; i < arr.length; i++) {
+
+            //add images to gallery
+
+            if (JSON.stringify(arr[i]['industry']).indexOf(filter) > 0) {
+
+                $('.bxslider2').append('<li> <a href="' + arr[i]['page-url'] + '"><img src="' + arr[i]['website-url'] + '" alt="' + arr[i]['client-name'] + '" /> </a></li>');
+            }
+        }
+
+        $('.flex-area h2').empty();
+
+        $('.service-detail.' + filter + ' h2').clone().insertAfter('.flex-area h1');
+
+        $('.bxslider2').bxSlider({
+            auto: true,
+                    mode: 'fade',
+                    speed: 4000
+        });
+
+    };
+
+    if (arr == null || arr.length == 0) {
+
+               //start ajax request
+
+        $.ajax({
+            url: "//mp-site.dynamic.marketpath.com/get/directory-data/4a743f66-194b-4ddb-aa5f-c148f51d55d8",
+                     //force to handle it as text
+                    dataType: "text",
+                    success: success
+        });
+    }
+    else {
+        successFilter(filter);
+    }
+
+}
+
+gallery();
+
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        auto: false,
+        mode: 'horizontal',
+        minSlides: 3,
+        maxSlides: 3,
+        moveSlides: 1
+    });
 });
-</script>
